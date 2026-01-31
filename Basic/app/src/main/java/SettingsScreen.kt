13769a3ed1bfccc33926885adf7b850a501c06e0
evangohh <s10258381@connect.np.edu.sg>
2026-01-31 12:36:00 +0800

    OptIn error - fixed

--- Basic/app/src/main/java/SettingsScreen.kt
+++ Basic/app/src/main/java/SettingsScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.OptIn
+import kotlin.OptIn
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
